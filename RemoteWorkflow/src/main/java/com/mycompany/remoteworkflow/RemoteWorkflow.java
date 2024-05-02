9e8ac2c0a691a8245d1448cd5b0d731a5f87fc3b
LizwiS <LizwiS@LIZWISLP.MPULEG.GOV.ZA>
2024-05-02 14:50:12 +0200

    My project commit

---
+++ RemoteWorkflow/src/main/java/com/mycompany/remoteworkflow/RemoteWorkflow.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.remoteworkflow;
+
+/**
+ *
+ * @author LizwiS
+ */
+public class RemoteWorkflow {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
